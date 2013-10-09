17eb179819dd9554feafcf5213e6ff86ac34c9d8
William Smyth May <wsmay1@uw.edu>
2013-10-08 21:31:47 -0700

    Header made.  Probably need to look at color schemes.

---
+++ common.php
@@ -0,0 +1,26 @@
+<?php function metaData() { ?>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<meta name="author" content="wsmay1">
+	<link rel="shortcut icon" href="img/logo.png">
+<?php } ?>
+
+<?php function heading() { ?>
+	<div id="heading">
+		<img src="img/logo.png" alt="pizzaLogo" id="logo">
+		<h1>Dawg Pizza</h1>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="menu.php">Menu</a></li>
+				<li><a href="about.php">About Us</a></li>
+			</ul>
+		</nav>
+	</div>
+<?php } ?>
+
+<?php function footing() { ?>
+	<div id="footing">
+		<p>&copy; Dawg Pizza 2013</p>
+	</div>
+<?php } ?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+	<title>Dawg Pizza</title>
+	<?php 
+	include 'common.php';
+	metaData();
+	?>
+	
+</head>
+<body>
+	<div id="contentArea">	
+		<?php heading() ?>
+	</div>
+</body>
+</html>
