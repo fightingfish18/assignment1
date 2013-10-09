9b30257c62377b2eddcba907a477752708c4cfe0
William Smyth May <wsmay1@uw.edu>
2013-10-08 22:03:32 -0700

    Experimenting.

--- index.php
+++ index.php
@@ -11,6 +11,17 @@
 <body>
 	<div id="contentArea">	
 		<?php heading() ?>
+		<p id="description">Awesome wood-fired pizza with fresh ingredients baked to order. Come in and eat, or call us for delivery!</p>
+		<div id="homePage">
+			<img src="img/pizza1.jpg" alt="pizza" id="pizzaPic">
+			<ul id="details">
+				<li>Our Business Hours: 10:00 AM - 11:00 PM, Monday through Saturday.</li>
+				<li>206-555-1212</li>
+				<li>4306 University Way NE, Seattle, WA, 98105</li>
+				<li><a href="mailto:dawgpizzaseattle@gmail.com">dawgpizzaseattle@gmail.com</a></li>
+				<li><a href="http://www.twitter.com/DawgPizzaSea">@DawgPizzaSea</a></li>
+			</ul>
+		</div>
 	</div>
 </body>
 </html>
